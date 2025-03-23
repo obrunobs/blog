1646d09aaf72c51a511509073650606b85f35aef
Bruno Souza <obrunobs@outlook.com>
2025-03-23 18:28:49 -0300

    created model comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_23_212046) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_23_212716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -21,4 +30,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_23_212046) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "posts"
 end
